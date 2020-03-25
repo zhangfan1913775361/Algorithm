35401625626bdb563288911fc030a59989bd7565
zhangfan <sa517486@mail.ustc.edu.cn>
2020-03-25 23:56:00 +0800

    股票最佳投资时间

--- src/yingyongti/BestTimetoBuyandSellStock.java
+++ src/yingyongti/BestTimetoBuyandSellStock.java
@@ -3,7 +3,6 @@ package yingyongti;
 股票买卖
 输入描述：一个浮点数数组，长度为N，都是增长率。  [-1.2 1.4 1.5 -1 1.3 1.3 -1.3 -1.2]
 输出描述：索引值表示时间。分别为买入索引（算在收益中），卖出索引（不算在收益中），以及收益值 [1,6,XX]
-
  */
 
 /**
@@ -13,6 +12,7 @@ package yingyongti;
 public class BestTimetoBuyandSellStock {
     public static void main(String[] args) {
         double [] n = new double[]{-1.2,1.4,1.5,-1,1.3,1.3,-1.3,-1.2};//定义一个浮点型数组
+
         System.out.println(BestTimetoBuyandSellStock.maxProfit(n));//用"类.方法名"，直接调用方法。
     }
     public static double maxProfit(double[] prices){
@@ -29,4 +29,10 @@ public class BestTimetoBuyandSellStock {
         }
         return profit;
     }
+//    public static double maxAddProfit(double[] addRate){
+//        for (int i=0;)
+//
+//        return date[i],date[j],pro
+
+//    }
 }

0177f93ab92bf42a53c0906796b9ef2c3269f95c
zhangfan <sa517486@mail.ustc.edu.cn>
2020-03-27 22:10:09 +0800

    字符串三个一组

---
+++ src/String/ThreeWord.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Scanner;
+
+/**
+ * 输入一行数字，每三个用逗号隔开
+ */
+public class ThreeWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String s = sc.nextLine();
+            while (s.length()>=3){
+                System.out.println(s.substring(0,3)+",");
+                s = s.substring(3);
+            }
+        }
+    }
+}

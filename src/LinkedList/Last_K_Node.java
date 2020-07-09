b525032cb7dc98e7693335a96467950bb6136ccd
zhangfan <sa517486@mail.ustc.edu.cn>
2020-07-09 18:45:59 +0800

    倒数第k个节点，用两个指针，一个快走一个慢走

--- src/LinkedList/Last_K_Node.java
+++ src/LinkedList/Last_K_Node.java
@@ -1,8 +1,10 @@
 package LinkedList;
 
+import LinkedList.Define.PrintListNode;
+
 /**
  * Created by zhangfan 2020/6/21
- * 剑指offer的一道简单题，链表倒数第k个结点
+ * 剑指offer的一道简单题，链表倒数第k个结点(凡是倒数的，都是一个先走一个后走)
  *
  */
 public class Last_K_Node {
@@ -10,7 +12,7 @@ public class Last_K_Node {
         int value; // 保存链表的值
         ListNode next; // 下一个结点
     }
-    public static ListNode getKthFromEnd(ListNode head,int k){
+    private static ListNode getKthFromEnd(ListNode head,int k){
         ListNode former = head,latter = head;
         //先让former走k步
         for (int i = 0; i < k; i++) {
@@ -24,6 +26,15 @@ public class Last_K_Node {
         }
         return latter;
     }
+
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.value + "->");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
 
     public static void main(String[] args) {
         ListNode head = new ListNode();
@@ -41,7 +52,8 @@ public class Last_K_Node {
         head.next.next.next.next = new ListNode();
         head.next.next.next.next.value = 5;
 
-        getKthFromEnd(head,2);
+        printList(head);
+        System.out.println("=========");
         System.out.println(getKthFromEnd(head,2).value);
     }
 
